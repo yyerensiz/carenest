97f0b7191fb085e1115de1e5beaec421cbf42447
yyerensiz <s01t002208@gmail.com>
2024-12-08 01:08:59 +0500

    Just checking new branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,3 @@
-// import 'package:carenest/screens/main_screen.dart';
-// import 'package:carenest/screens/signup_screen.dart';
-// import 'package:flutter/material.dart';
-// import 'package:firebase_core/firebase_core.dart';
-
-// //screens
-// import 'screens/login_screen.dart';
-// import 'screens/home_screen.dart';
-// import 'screens/specialist_details.dart';
-
-// //
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Helper App',
-//       theme: ThemeData(primarySwatch: Colors.blue),
-//       initialRoute: '/',
-//       routes: {
-//         '/': (context) => LoginScreen(),
-//         '/signup': (context) => SignupScreen(),
-//         '/main': (context) => MainScreen(),
-//       },
-//     );
-//   }
-// }
-
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
